ed75b4ade10f93735091e70b164562e47d9825a8
DMrFrost <31308778+DMrFrost@users.noreply.github.com>
2017-08-24 12:45:42 -0700

    Delete linkConfig.php

--- map/sqlFunctions/linkConfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-  DEFINE('SERVER_NAME', 'localhost'); //Database server name
-  DEFINE('USER_NAME', 'sumoftru_public'); //Database user name
-  DEFINE('PWD', 'Diskman303_'); //Database user password
-  DEFINE('DB_NAME', 'sumoftru_map'); //Database name
-?>
